23f2fd965335c005cfbd5bd7548d1097193a0eae
arnav kumar <joinarnavkumar@gmail.com>
2024-10-13 20:12:17 +0530

    craeting repo

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-3xl text-red-700">Hello</h1>
+    </div>
+  );
+}
